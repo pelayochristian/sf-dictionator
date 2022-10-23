1cd81d71f5f5c2dc0cee70fd83683947ed018c62
Christian Pelayo <pelayochristian.dev@gmail.com>
2022-10-23 23:07:38 +0800

    add sign out icon

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -68,10 +68,34 @@ const Header = () => {
                             >
                                 Github
                             </Dropdown.Item>
-                            <Dropdown.Item>Hasnode</Dropdown.Item>
+                            <Dropdown.Item
+                                onClick={() =>
+                                    router.push(
+                                        "https://github.com/pelayochristian/salesforce-next-auth"
+                                    )
+                                }
+                            >
+                                Hasnode
+                            </Dropdown.Item>
                             <Dropdown.Divider />
                             <Dropdown.Item onClick={() => signOut()}>
-                                Sign out
+                                <div className="flex gap-2">
+                                    <svg
+                                        className="h-6 w-6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                                        ></path>
+                                    </svg>
+                                    <p>Sign out</p>
+                                </div>
                             </Dropdown.Item>
                         </Dropdown>
                         <Navbar.Toggle />
