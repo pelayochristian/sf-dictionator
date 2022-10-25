b5dc856a8c2720dd2a1048858812feadac0a49f2
Christian Pelayo <pelayochristian.dev@gmail.com>
2022-10-25 18:06:01 +0800

    exportFile

--- src/components/ExportPOC.tsx
+++ src/components/ExportPOC.tsx
@@ -1,34 +1,25 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { read, utils, writeFileXLSX } from "xlsx-js-style";
+import { utils, writeFile } from "xlsx-js-style";
 
 const ExportPOC = () => {
-    /* the component state is an array of presidents */
-    const [pres, setPres] = useState([]);
-
-    /* Fetch and update the state once */
-    useEffect(() => {
-        (async () => {
-            const f = await (
-                await fetch("https://sheetjs.com/pres.xlsx")
-            ).arrayBuffer();
-            const wb: any = read(f); // parse the array buffer
-            const ws = wb.Sheets[wb.SheetNames[0]]; // get the first worksheet
-            const data: any = utils.sheet_to_json(ws); // generate objects
-            setPres(data); // update state
-        })();
-    }, []);
-
+    const dataTMP = [
+        { firstName: "Christian", lastName: "pelayo" },
+        { firstName: "Christian", lastName: "pelayo" },
+        { firstName: "Christian", lastName: "pelayo" },
+    ];
     /* get state data and export to XLSX */
-    const exportFile = useCallback(() => {
-        const ws = utils.json_to_sheet(pres);
-        const wb = utils.book_new();
-        utils.book_append_sheet(wb, ws, "Data");
-        writeFileXLSX(wb, "SheetJSReactAoO.xlsx");
-    }, [pres]);
+    const exportFile = (data: { firstName: string; lastName: string }[]) => {
+        const worksheet = utils.json_to_sheet(data);
+        const workbook = utils.book_new();
+        utils.book_append_sheet(workbook, worksheet, "Sheet1");
+        //let buffer = write(workbook, { bookType: ", type: "buffer" });
+        //write(workbook, { bookType: ", type: "binary" });
+        writeFile(workbook, "DataSheet.xlsx");
+    };
 
     return (
         <>
-            <button onClick={exportFile}>Export XLSX</button>
+            <button onClick={() => exportFile(dataTMP)}>Export XLSX</button>
         </>
     );
 };
